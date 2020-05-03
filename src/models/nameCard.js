76c34e9f4bbded00d0c245a04077744f20a71232
michaelsihombing <sntmcl1@gmail.com>
2020-05-04 01:00:05 +0700

    [MIKE]add new feature

--- src/models/nameCard.js
+++ src/models/nameCard.js
@@ -8,6 +8,20 @@ const imagekit = new Imagekit({
    urlEndpoint: process.env.urlEndpoint
 })
 
+
+const randomImage = [
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__5___gVErlfkr.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__4__bcJrAnNDS.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__3__G3mwd4sOJt.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__2__rzdmaMNz8e.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__1__IrwwDBdiP.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM_QsD9fMMl-.jpeg"
+]
+
+function defaultImage() {
+   return randomImage[Math.floor(Math.random() * randomImage.length)]
+}
+
 const nameCardSchema = new Schema({
    userId: {
       type: Schema.Types.ObjectId,
@@ -24,7 +38,8 @@ const nameCardSchema = new Schema({
    },
 
    image: {
-      type: String
+      type: String,
+      default: defaultImage()
    },
 
    fullname: {
--- src/models/user.js
+++ src/models/user.js
@@ -9,6 +9,20 @@ const imagekit = new Imagekit({
    urlEndpoint: process.env.urlEndpoint
 });
 
+
+const randomImage = [
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__5___gVErlfkr.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__4__bcJrAnNDS.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__3__G3mwd4sOJt.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__2__rzdmaMNz8e.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM__1__IrwwDBdiP.jpeg",
+   "https://ik.imagekit.io/m1ke1magek1t/default_image/WhatsApp_Image_2020-02-26_at_5.42.11_PM_QsD9fMMl-.jpeg"
+]
+
+function defaultImage() {
+   return randomImage[Math.floor(Math.random() * randomImage.length)]
+}
+
 const userSchema = new Schema({
    email: {
       type: String,
@@ -25,7 +39,8 @@ const userSchema = new Schema({
    },
 
    image: {
-      type: String
+      type: String,
+      default: defaultImage()
    },
 
    fullname: {
